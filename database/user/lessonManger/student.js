7f42e1683744842fadfe9f65ed58c992d7640b01
Christian List <dragondriver8@outlook.com>
2025-04-27 19:29:31 -0400

    Making the lesson for the next month for studetns

--- database/user/lessonManger/student.js
+++ database/user/lessonManger/student.js
@@ -1,30 +1,85 @@
-const dbClient = require("../../db.js");
+import { dbClient } from "../../db.js";
 const dateVar = require("../../../variables/datas.js");
 const lessonQury = `
   INSERT INTO lesson (student_ID,lesson_start, lesson_date)
   VALUES ($1, $2, $3)
+  RETURNING id
+
 `; // This is setup for insert lesson in the table called lesson to create a new lesson.
 
-// Define the weekday (0 for Sunday, 1 for Monday, ..., 6 for Saturday)
 async function createLessonMonth(studentID, numberOfLesson) {
   const currentDate = dateVar.currentDate;
   const nextMonth = dateVar.nextMonth;
 
   const studentPropertys = await getStudentPropertys(studentID);
   //console.log(studentPropertys);
-  const lessonNumber = studentPropertys.lesson_ID;
-  if (!lessonNumber) {
+  const lessonNumberList = studentPropertys.lesson_id;
+  if (!lessonNumberList) {
     console.log(`No lesson ID found for student with ID: ${studentID}`);
     return null;
   }
 
-  console.log("Lesson Number:", lessonNumber);
-  console.log("Lesson Number:", lessonNumber.length);
-  if (numberOfLesson > lessonNumber.length) {
+  console.log("Lesson Number:", lessonNumberList);
+  console.log("Lesson Number:", lessonNumberList.length);
+  if (numberOfLesson > lessonNumberList.length - 1) {
     return null;
   }
-  // Create Leson until next Month 
-  return true;
+  // Create Lesson
+  const lessonNumber = lessonNumberList[numberOfLesson];
+  const lessonInfo = await getLessonInfo(lessonNumber);
+  console.log(lessonInfo)
+  const lessonInfoArray = {
+    lessonStart: lessonInfo["lesson_start"],
+    lessonLenght: lessonInfo["lesson_length"]
+  }
+
+  let lessonDates = [];
+  for (let day = new Date(currentDate); day.getTime() < nextMonth.getTime(); day.setDate(day.getDate() + 1)) {
+    if (day.getDay() == lessonInfo["lesson_weekday"]) {
+      lessonDates = [...lessonDates, {
+        year: day.getFullYear(),
+        month: day.getMonth(),
+        date: day.getDate(),
+        done: false
+      }];
+    }
+  }
+  if (lessonInfo["up_comming_lessons_id"] != null) {
+    for (let lesson_ID = 0; lesson_ID < lessonInfo["up_comming_lessons_id"].length; lesson_ID++) {
+      const element = lessonInfo["up_comming_lessons_id"][lesson_ID];
+      const results = getLessonDate(element);
+      if (ifResultsThere(results)) {
+      }
+    }
+  }
+
+  for (let listOfDates = 0; listOfDates < lessonDates.length; listOfDates++) {
+    if (!lessonDates[listOfDates]["done"]) {
+      console.log("Not Done".lessonDates)
+      const lesson = lessonDates[listOfDates];
+
+      const formattedDate = `${lesson["year"]}-${lesson["month"].toString().padStart(2, '0')}-${lesson["date"].toString().padStart(2, '0')}`;
+      const create_Lesson_resutls = await dbClient.query(lessonQury, [studentID, lessonInfoArray["lessonStart"], formattedDate]);
+      console.log("Comple Time")
+      if (ifResultsThere(create_Lesson_resutls)) {
+        console.log("Complete Correct");
+      } else {
+        console.log("Error In resutls")
+      }
+      console.log(create_Lesson_resutls.rows[0].id)
+    }
+
+  }
+
+  console.log(lessonDates);
+}
+
+async function ifResultsThere(results) {
+  if (results.rows.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 async function getStudentPropertys(studentID) {
@@ -45,17 +100,16 @@ async function getStudentPropertys(studentID) {
   }
 }
 
-async function getLessonInfo(studentID) {
+async function getLessonInfo(lesson_id) {
   try {
     const results = await dbClient.query(
-      "SELECT * FROM lessonDefualt WHERE student_ID = $1",
-      [studentID]
+      "SELECT * FROM lessonDefault WHERE id = $1",
+      [lesson_id]
     );
 
     if (results.rows.length > 0) {
       return results.rows[0];
     } else {
-      console.log(`No default lesson found for student with ID: ${studentID}`);
       return null;
     }
   } catch (error) {
@@ -85,8 +139,17 @@ async function seeLessonDate(date, CheckLessonID) {
     return -1;
   }
 }
+
+async function getLessonDate(lesson_id) {
+  const results = await dbClient.query(
+    `SELECT (id, lesson_date) FROM lesson WHERE id = $1`,
+    [lesson_id]
+  )
+  if (reuslt.rows.length > 0) {
+    return results.rows[0];
+  }
+}
 // Example call:
-createLessonMonth(1, 1);
+await createLessonMonth(1, 0);
 
-createLessonMonth(2, 1);
-
+//createLessonMonth(2, 1)
