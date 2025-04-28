7da304c069edf6fd90fe4dc0b765813f921cfac5
Christian List <dragondriver8@outlook.com>
2025-04-28 18:07:57 -0400

    Update db.js
    Chaining the require to from statments

--- database/db.js
+++ database/db.js
@@ -1,16 +1,19 @@
 // This file sets up a connection to a PostgreSQL database using the 'pg' package.
+
 import dotenv from 'dotenv';
 dotenv.config();
+
 // Import the 'pg' package to interact with PostgreSQL
-const { Client } = require("pg");
+import pkg from 'pg';
+const { Client } = pkg;
 
 // Create a new client instance using environment variables
 const client = new Client({
-  user: process.env.DB_USER, // DB username from .env
-  host: process.env.DB_HOST, // DB host from .env
+  user: process.env.DB_USER,     // DB username from .env
+  host: process.env.DB_HOST,     // DB host from .env
   database: process.env.DB_NAME, // DB name from .env
   password: process.env.DB_PASSWORD, // DB password from .env
-  port: process.env.DB_PORT, // DB port from .env
+  port: process.env.DB_PORT,     // DB port from .env
 });
 
 // Connect to the PostgreSQL database
@@ -23,23 +26,6 @@ client
     console.error("Connection error", err.stack);
   });
 
-// Optional: Create a table if it doesn't exist (you might want to run this separately just once)
-// const createTable = `
-// CREATE TABLE IF NOT EXISTS users (
-//   id INTEGER PRIMARY KEY,
-//   name VARCHAR(255) NOT NULL,
-//   userName VARCHAR(20) NOT NULL,
-//   password VARCHAR(20) NOT NULL,
-//   birthDay INTEGER NOT NULL,
-//   birthMonth INTEGER NOT NULL,
-//   birthYear INTEGER NOT NULL,
-//   teacher BOOLEAN DEFAULT FALSE
-// )`;
-
-// client
-// 	.query(createTable)
-// 	.then(() => console.log("Table created or already exists"))
-// 	.catch((err) => console.error("Error creating table", err.stack));
-
 // Export the client so other parts of your app can use it
-export const dbClient = { client };
+export default client;
+
